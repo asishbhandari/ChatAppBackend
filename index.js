0e613db27c456612658ea3f68b110b9fea3182cd
asishbhandari <asish.bhandari17@gmail.com>
2024-09-03 18:57:41 +0530

    Sockets connection check

--- index.js
+++ index.js
@@ -34,8 +34,6 @@ app.use("/api/message", messageRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-module.exports = app;
-
 const PORT = process.env.PORT || 5001;
 
 // sockets logics
@@ -62,6 +60,7 @@ io.on("connection", (socket) => {
   });
 });
 
+module.exports = app;
 server.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
