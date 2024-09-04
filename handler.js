ecf37122f407bed14204c5d2f738ada20f2bc4cf
asishbhandari <asish.bhandari17@gmail.com>
2024-09-04 11:06:55 +0530

    Socket handler removed

--- handler.js
+++ handler.js
@@ -1,6 +1,10 @@
-const { app, io } = require("./index");
+const { app } = require("./index");
 const serverless = require("serverless-http");
 exports.expressApp = serverless(app);
-exports.connectionHandler = async (event, context) => {
-  console.log(event, context);
-};
+// exports.connectionHandler = async (event) => {
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify("success"),
+//   };
+//   return response;
+// };
