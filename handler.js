3b94fb697b55394cf2fc7812864bc57493dcfb43
asishbhandari <asish.bhandari17@gmail.com>
2026-03-01 11:29:59 +0530

    some changes

--- handler.js
+++ handler.js
@@ -1,10 +1,3 @@
 const { app } = require("./index");
 const serverless = require("serverless-http");
 exports.expressApp = serverless(app);
-// exports.connectionHandler = async (event) => {
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify("success"),
-//   };
-//   return response;
-// };
